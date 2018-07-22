a8d610a516240db5c98c092a93479a6758e642eb
Sivamuthu Kumar <sivamuthukumar07@gmail.com>
2018-07-22 09:55:22 -0400

    Update deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular'
 
 @NgModule({
   declarations: [
@@ -11,9 +12,10 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    AmplifyAngularModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [AmplifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports.js';
 
 if (environment.production) {
   enableProdMode();
 }
+
+Amplify.configure(aws_exports);
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
